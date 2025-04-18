a92f9c6959fab9ad8e428a56cfb4f43fd8cbc3cf
smhasanjamil <smhasanjamil23@gmail.com>
2025-04-18 16:11:22 +0600

    Adddes Zod validation middleware

--- server/src/app/modules/user/user.route.ts
+++ server/src/app/modules/user/user.route.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { userControllers } from "./user.controller";
+import validateRequest from "../../middleware/validateRequest";
+import { userValidations } from "./user.validation";
 
 const router = Router();
 
-router.post("/create-user", userControllers.createUser);
+router.post(
+  "/create-user",
+  validateRequest(userValidations.createUserValidationSchema),
+  userControllers.createUser
+);
 
 export const UserRoutes = router;
--- server/src/app/modules/user/user.validation.ts
+++ server/src/app/modules/user/user.validation.ts
@@ -1,16 +1,36 @@
 import { z } from "zod";
 
-const userSchemaValidation = z.object({
-  name: z.string().min(1, "Name is required"),
-  email: z.string().email("Invalid email format"),
-  phoneNumber: z
-    .string()
-    .min(10, "Phone number must be at least 10 digits")
-    .max(14, "Phone number must be at most 14 digits"),
-  password: z.string().min(6, "Password must be at least 6 characters long"),
-  role: z.enum(["user", "admin"]).optional().default("user"),
+const createUserValidationSchema = z.object({
+  body: z.object({
+    name: z
+      .string({
+        required_error: "Name is required",
+      })
+      .min(1, "Name is required"),
+
+    email: z
+      .string({
+        required_error: "Email is required",
+      })
+      .email("Invalid email format"),
+
+    phoneNumber: z
+      .string({
+        required_error: "Phone number is required",
+      })
+      .min(10, "Phone number must be at least 10 digits")
+      .max(14, "Phone number must be at most 14 digits"),
+
+    password: z
+      .string({
+        required_error: "Password is required",
+      })
+      .min(6, "Password must be at least 6 characters long"),
+
+    role: z.enum(["user", "admin"]).optional().default("user"), // Optional with default
+  }),
 });
 
-export const userValidation = {
-  userSchemaValidation,
+export const userValidations = {
+  createUserValidationSchema,
 };
