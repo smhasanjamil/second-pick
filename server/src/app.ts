ee85ce5755b860eaaded8148768a249d86305d7a
smhasanjamil <smhasanjamil23@gmail.com>
2025-04-20 13:47:36 +0600

    Initialize Next.js client with shadcn ui

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,7 +12,7 @@ const app: Application = express();
 // parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: ["http://localhost:5173"] }));
+app.use(cors({ origin: ["http://localhost:5173", "http://localhost:3000"] }));
 
 // application routes
 // app.use("/api/v1", router);
